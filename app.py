204176f9a91d3cc8755db10a771f65a0eda0e9c3
etech888919-hash <etech888919@gmail.com>
2026-01-14 13:25:11 +0530

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 import requests
 import time
+import os
+
 
 app = Flask(__name__)
 
@@ -73,5 +75,9 @@ def send_reply(phone, text):
     )
 
 
+
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
